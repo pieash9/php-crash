37506b78c9de2b5f78ff285a4c6e66a695076713
pieash9 <pieash9@gmail.com>
2025-01-31 22:48:22 +0600

    added array

---
+++ 06_functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+// function registerUser($email){
+//     echo $email . " registered";
+// }
+// registerUser("pieash@gmail.com")
+
+// function sum($n1 = 8, $n2 = 9){
+//     return $n1 + $n2;
+// }
+
+// $number = sum();
+
+// echo $number
+
+$multiply = fn($n1, $n2) => $n1 * $n2;
+
+echo $multiply(2, 3);
+
+?>
---
+++ 07_array_function.php
@@ -0,0 +1,28 @@
+<?php
+
+$fruits = ["apple", "banana", "orange", "kiwi", "grape"];
+
+// get length
+// echo count($fruits);
+// var_dump(in_array("grape", $fruits));
+
+
+
+// push
+// $fruits[] = 'mango';
+// array_push($fruits, "Title");
+// array_unshift($fruits, "lemon");
+
+// remove
+// array_pop($fruits);
+// array_shift($fruits);
+// unset($fruits[1]);
+
+// split into 2 chunks
+// $chunk_array = array_chunk($fruits, 3);
+
+
+print_r($chunk_array)
+
+
+?>
